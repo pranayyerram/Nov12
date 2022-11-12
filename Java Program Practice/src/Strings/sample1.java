0052d0e769f629ceedc477308f448f6733253beb
pranayyerram <pranayyerram@gmail.com>
2022-11-12 18:20:01 +0530

    First Commit

---
+++ Java Program Practice/src/Strings/sample1.java
@@ -0,0 +1,91 @@
+package Strings;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+//		// 1. Get char at given index
+//		String org="Java Programs!";
+//		System.out.println("Original Value: "+org);
+//		
+//		int index1=org.charAt(0);
+//		int index2=org.charAt(5);
+//		int index3=org.charAt(7);
+//		
+//		System.out.println((char)index1+" "+(char)index2+" "+(char)index3);
+		
+		//2. To check if strings are equal
+//		String x1="Hello world!!";
+//		String x2="Hello World!!";
+//		
+//		int result=x1.compareToIgnoreCase(x2);
+//		System.out.println(result);
+	
+		// 3. Concate 2 strings at end of other string
+//		String x1="Hello!";
+//		String x2="To Everyone";
+//		
+//		String x3=x1.concat(x2);
+//		System.out.println(x3);
+		
+		// 4. if a given string contains the specified sequence of char values.
+//		String x="Sun Rises in the east";
+//		String y="in ";
+//		System.out.println(x.contains(y));
+		
+		//5. Compare given strings
+//		String x="Java Example";
+//		String y="Jjava Example";
+//		System.out.print(x.equals(y));
+//		System.out.println(x.contentEquals(y));
+		
+		//6. Take value from array to string
+//		char[] arr_num=new char[] {'1','2','3','4'};
+//		String x=String.copyValueOf(arr_num, 1, 3);
+//		
+//		System.out.print("\nThis book contains: "+x+"\n");
+//		
+		//7. check string ends and starts with given value
+//		String x="This is life";
+//		String y="e ";
+//		System.out.println(x.endsWith(y));
+//		System.out.println(x.startsWith("This"));
+//		System.out.print(x.startsWith(x, 0));
+		
+		//8. Create unique hashcode
+//		String x="Python Java";
+//		System.out.print(x.hashCode());
+		
+		//9. Get index of character in string
+//		String x="Pranay";
+//		System.out.println(x.indexOf("y"));
+//		System.out.println(x.indexOf("a", 3));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
